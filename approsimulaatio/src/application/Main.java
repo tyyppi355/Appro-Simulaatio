48a52bbf8e3302c392910ef30b1118113a615d73
Arbit3r <rikukoski2@gmail.com>
2022-03-07 19:43:02 +0200

    fix

--- approsimulaatio/src/application/Main.java
+++ approsimulaatio/src/application/Main.java
@@ -9,10 +9,6 @@ public class Main { // Simulaattorin käynnistyspääohjelma
 	
 	public static void main(String args[]) {
 		
-		Tietokanta yeet = new Tietokanta();
-		Palvelupiste p = new Palvelupiste("Dondo",60.2219389088197,24.81443193360821);
-		yeet.createMatka(new Matka("dondo",655,255));
-		yeet.createPalvelupiste(p);
 		view.SimulaattorinGUI.main(args);
 		
 	}
